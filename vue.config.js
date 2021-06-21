9737f0cadc7ed121ced0e0735247bd98bec8abc1
Marviuz <marviuzmarvieluz@gmail.com>
2021-06-21 13:38:31 +0800

    add sitemap
    add sitemap using sitemap-webpack-plugin package

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+const paths = ['/']
 
 module.exports = {
   transpileDependencies: [
@@ -7,11 +10,12 @@ module.exports = {
   ],
   configureWebpack: {
     plugins: [
+      new SitemapPlugin({ base: 'https://marviuz.github.io', paths }),
       new PrerenderSPAPlugin({
         // Required - The path to the webpack-outputted app to prerender.
         staticDir: path.join(__dirname, 'dist'),
         // Required - Routes to render.
-        routes: ['/'],
+        routes: paths,
       })
     ]
   }
